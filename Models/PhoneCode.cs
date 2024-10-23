21e551704d4323d9f36d482616efec478d32d41a
gkarageorgos <97295445+gkarageorgos@users.noreply.github.com>
2024-10-23 16:58:56 +0300

    Add files via upload

---
+++ Models/PhoneCode.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhoneBook.Models
+{
+    public class PhoneCode
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        [DisplayName("Κωδικός")]
+        public string Name { get; set; } = null!;
+    }
+}

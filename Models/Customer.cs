ffcda8aeb18dae74b4d01eed76ac1fbc828c26fc
gkarageorgos <97295445+gkarageorgos@users.noreply.github.com>
2024-10-24 17:14:58 +0300

    Add files via upload

---
+++ Models/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhoneBook.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        [DisplayName("Όνομα")]
+        public string? Name { get; set; }
+
+        [StringLength(100)]
+        [DisplayName("Επίθετο")]
+        public string? Surname {  get; set; }
+
+        [StringLength(200)]
+        [DisplayName("Σημειώσεις")]
+        public string? Notes { get; set; }
+
+        public ICollection<Phone> Phones { get; set; } = [];
+
+        public ICollection<Email> Emails { get; set; } = [];
+
+        public int? CompanyId { get; set; }
+        [DisplayName("Εταιρεία")]
+        public Company? Company { get; set; }
+    }
+}
---
+++ Models/PhoneBookType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhoneBook.Models
+{
+    public class PhoneBookType
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DisplayName("Τύπος")]
+        public string Name { get; set; } = null!;
+    }
+}
